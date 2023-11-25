fc62aca4c5f4a04bf3835ca28afc2d412899b224
lance <lanceparker2001@gmail.com>
2023-11-26 01:53:05 +0530

    Adding AdOnLeft Component 1

--- src/Components/Ads/AdOnLeft.jsx
+++ src/Components/Ads/AdOnLeft.jsx
@@ -3,21 +3,19 @@ import React from "react";
 
 const AdOnLeft = () => {
   return (
-    <div
-      dangerouslySetInnerHTML={{
-        __html: `<amp-ad
-    width="100vw"
-    height="320"
-    type="adsense"
-    data-ad-client="ca-pub-1113294681015194"
-    data-ad-slot="6132635833"
-    data-auto-format="rspv"
-    data-full-width=""
-  >
-    <div overflow=""></div>
-  </amp-ad>`,
-      }}
-    ></div>
+    <Box>
+      <amp-ad
+        width="100vw"
+        height="320"
+        type="adsense"
+        data-ad-client="ca-pub-1113294681015194"
+        data-ad-slot="6132635833"
+        data-auto-format="rspv"
+        data-full-width=""
+      >
+        <div overflow=""></div>
+      </amp-ad>
+    </Box>
   );
 };
 
